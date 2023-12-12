befb1f0d5c3d88ff3362667c012307930960e4de
Sahishnu Jadhav <96820614+Swerve145@users.noreply.github.com>
2023-12-12 18:18:42 +0000

    Add files via upload

---
+++ P117/CreateVideo.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+
+
+path = "Images/"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        #print(file_name)
+               
+        images.append(file_name)
+        
+count = (len(images))
+
+frame = cv2.imread(images[0])
+height,width,channels = frame.shape
+size = (width,height)
+print(size)
+
+out = cv2.VideoWriter("project.avi",cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+
+
+for i in range(0,count-1):
+    frame = cv2.imread(images[i])
+    cv2.imshow("o1", frame)
+    if cv2.waitKey(20) == 32:
+        break
+    out.write(frame)
+
+out.release()
+print("done")
+
+cv2.destroyAllWindows()
